59c933c5768423bc175c8714ef4ec79a9e4f97d0
Sergei Soletskii <serey19992@yandex.ru>
2026-05-02 21:48:15 +0300

    Second commit

--- task.js
+++ task.js
@@ -1,40 +1,36 @@
 const formContainer = document.querySelector('#signin__form');
 const url = "https://students.netoservices.ru/nestjs-backend/auth";
-let xhr = new XMLHttpRequest();
+
 const userName = document.getElementById("user_id");
 const welcome = document.querySelector(".welcome");
 const signin = document.getElementById("signin");
 
 
-if (localStorage.getItem("user_id")) {
-    signin.classList.remove('signin_active');
-    userName.innerText = localStorage.getItem("user_id");
-    welcome.classList.add('welcome_active');
-
-}
-
-
 formContainer.addEventListener('submit', (e) => {
     e.preventDefault();
+    let xhr = new XMLHttpRequest();
 
     let formData = new FormData(formContainer);
     xhr.open('POST', url, true);
-
+    xhr.responseType = 'json'
     xhr.send(formData);
-    xhr.addEventListener('readystatechange', () => {
-        if (xhr.readyState === 4 && xhr.status === 201) {
-            const serverResponse = JSON.parse(xhr.responseText);
-            const userId = serverResponse["user_id"];
-            if (!serverResponse["success"]){
-                alert("Ошибка логин пароль");
-                setTimeout(() => {
-                    window.location.reload(); },
-                    1000);
-                return;
-            }
-            localStorage.setItem("user_id", userId);
-            location.reload();
+    xhr.addEventListener('load', () => {
+        // const serverResponse = xhr;
+        const userId = xhr.response["user_id"];
+        if (!xhr.response["success"]){
+            alert("Ошибка логин пароль");
+            formContainer.reset();
+            return;
         }
+        localStorage.setItem("user_id", userId);
+        signin.classList.remove('signin_active');
+        userName.innerText = localStorage.getItem("user_id");
+        welcome.classList.add('welcome_active');
+        formContainer.reset();
+
+
     })
 })
+
+
 
